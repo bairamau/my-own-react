7f98c59b49bdbfae086d607e19bc3fd413f572c0
bairamau <bairamau.z@gmail.com>
2024-07-28 08:26:59 +0400

    Step IV: Fibers

--- didact.js
+++ didact.js
@@ -20,24 +20,30 @@ function createTextElement(text) {
   }
 }
 
-// In reality React doesn't create empty arrays when there are no children
-// and doesn't wrap plain text into special TEXT_ELEMENT
-// but we will do that to sacrifice performance for the sake of simplicity
+function createDom(fiber) {
+  // In reality React doesn't create empty arrays when there are no children
+  // and doesn't wrap plain text into special TEXT_ELEMENT
+  // but we will do that to sacrifice performance for the sake of simplicity
 
-function render(element, container) {
   const dom =
-    element.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(element.type)
+    fiber.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(fiber.type)
 
-  for (let prop in element.props) {
+  for (let prop in fiber.props) {
     if (prop === "children") continue
-    dom[prop] = element.props[prop]
+    dom[prop] = fiber.props[prop]
   }
 
-  container.appendChild(dom)
+  return dom
+}
 
-  element.props.children.forEach((child) => render(child, dom))
+function render(element, container) {
+  // set the next unit of work to the root of the fiber tree
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  }
 }
 
 let nextUnitOfWork = null
@@ -55,8 +61,62 @@ function workLoop(deadline) {
   requestIdleCallback(workLoop)
 }
 
-function performUnitOfWork(nextUnitOfWork) {
+requestIdleCallback(workLoop)
 
+function performUnitOfWork(fiber) {
+
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+
+  while (index < elements.length) {
+    const element = elements[index]
+
+    // create a new fiber for each child
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+
+    // add it to the fiber tree setting it as a child on the ancestor or
+    // as a sibling on the previous sibling
+    // depending on whether it's the first child or not
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
+  }
+
+  // search for the next unit of work
+  // first try child
+  // then sibling
+  // then sibling of parent
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+
+    nextFiber = nextFiber.parent
+  }
 }
 
 const Didact = {
